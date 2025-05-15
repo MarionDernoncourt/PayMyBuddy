4f5e3d6034b3c8dd9fbb0ba44c2a03173b4c1626
MarionDernoncourt <marion.dernon@gmail.com>
2025-05-15 15:51:04 +0100

    Implementation model + Repository de User et Transaction

---
+++ src/main/java/com/paymybuddy/model/User.java
@@ -0,0 +1,106 @@
+package com.paymybuddy.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "username")
+	private String username;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "password")
+	private String password;
+
+	@ManyToMany(
+			fetch = FetchType.LAZY)
+			
+	@JoinTable(
+			name = "user_user", 
+			joinColumns = @JoinColumn(name = "user_id"), 
+			inverseJoinColumns = @JoinColumn(name = "connected_user_id"))
+	List<User> friends = new ArrayList<>();
+
+	@ManyToMany(
+			mappedBy = "friends")
+	private List<User> friendsOf = new ArrayList<>();
+	
+	public void addFriend(User user) {
+		friends.add(user);
+		user.getFriends().add(this);
+	}
+	
+	public void removeFriend(User user) {
+		friends.remove(user);
+		user.getFriends().remove(this);
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<User> getFriends() {
+		return friends;
+	}
+
+	public void setFriends(List<User> friends) {
+		this.friends = friends;
+	}
+
+	public List<User> getFriendsOf() {
+		return friendsOf;
+	}
+
+	public void setFriendsOf(List<User> friendsOf) {
+		this.friendsOf = friendsOf;
+	}
+
+	
+}
